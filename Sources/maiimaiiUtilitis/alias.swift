19dab3e0503c322a31e00d795929dea8a21c77e9
HIroshi Imai <im@i-hiroshi.com>
2024-02-12 21:19:49 +0900

    aliasの実験

---
+++ Sources/maiimaiiUtilitis/alias.swift
@@ -0,0 +1,13 @@
+//
+//  alias.swift
+//  libtest
+//
+//  Created by Imai Hiroshi on 2/12/24.
+//
+
+import UIKit
+import maiimaiiUtilitis
+
+class alias: NSObject {
+	typealias x = maiimaiiUtilitis.MT
+}

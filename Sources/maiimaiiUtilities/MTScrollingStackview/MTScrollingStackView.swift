36234c38ef34d417fe52be68d6f59dd9e0e51639
HIroshi Imai <im@i-hiroshi.com>
2025-01-03 03:47:52 +0900

    コメント追加

--- Sources/maiimaiiUtilities/MTScrollingStackview/MTScrollingStackView.swift
+++ Sources/maiimaiiUtilities/MTScrollingStackview/MTScrollingStackView.swift
@@ -23,6 +23,9 @@ public class MTScrollingStackView: UIView {
 	
 	func loadNib(){
 		
+		// 直接プロジェクトに打ち込む場合はこちら
+		// if let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView
+		
 		if let view = Bundle.module.loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView {
 			view.frame = self.bounds
 			self.addSubview(view)
